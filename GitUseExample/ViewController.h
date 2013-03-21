b360a612190b117a316529d214e8de85cabdb46e
oooffspring <o.offspring131@gmail.com>
2013-03-21 12:58:45 +0800

    Initial Commit

---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by offspring on 13-3-21.
+//  Copyright (c) 2013年 offspring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitUseExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitUseExample
+//
+//  Created by offspring on 13-3-21.
+//  Copyright (c) 2013年 offspring. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
